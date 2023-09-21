25481aabec7c90c4539c1822a8e3d2862675e091
Sergio <spinfinite07@gmail.com>
2023-09-21 00:58:32 -0600

    UTTextField functionality

--- UITEXTFIELDDEMO/ViewController.swift
+++ UITEXTFIELDDEMO/ViewController.swift
@@ -7,13 +7,48 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    
+    @IBOutlet weak var firstTextField: UITextField!
+    
+    @IBOutlet weak var secondTextField: UITextField!
+    
+    
+    @IBAction func loginTapped(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTextFields()
+        
         // Do any additional setup after loading the view.
     }
-
+    
+    private func configureTextFields() {
+        firstTextField.delegate = self
+        secondTextField.delegate = self
+    }
+    
+    private func configureTapGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTap))
+        view.addGestureRecognizer(tapGesture)
+        
+    }
+    
+    @objc func handleTap() {
+        print("Handle tap was called")
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    
 
 }
 
